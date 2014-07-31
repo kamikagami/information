f7d2b3d514050339459224ebbd6e6d83ef752719
admin <admin@vagrant-centos65.vagrantup.com>
2014-07-31 12:45:17 +0000

    added flag -g

--- information.sh
+++ information.sh
@@ -56,9 +56,14 @@
     fi
     
     #gFlag
-   # if [ -n "$gFlag" ]; then
-   #     
-   # fi
+    if [ -n "$gFlag" ]; then
+       users=$(awk -F: '{ print $1}' /etc/passwd)
+       for user in $users;do
+         echo Username: $user
+         echo Groups: `id $user -nG | awk '{n=split($0, a);for(i in a) if (i<n){ print a[i]","} else {print a[i]} }'`
+         echo  
+       done
+    fi
 
     #lFlag
     if [ -n "$lFlag" ]; then
